b240956baa1ea56d49895ffcaf522cf78676fc39
Tanishqmalu <54908702+Tanishqmalu@users.noreply.github.com>
2021-04-16 03:26:01 +0530

    Adding component - ContentBox

---
+++ components/contentBox.js
@@ -0,0 +1,53 @@
+import "./styles.css";
+
+export default function Boxx() {
+  return (
+    < div className = "wrapper0">
+      <div className="wrapper1">
+      <p id="h0">Hub Governing Body</p>
+      <p id="h1">
+        The Indian Institute of Technology Patna has constituted a Hub Governing
+        Body for Technology Innovation Hub (TIH) under National Mission on
+        Interdisciplinary Cyber Physical Systems (NM-ICPS).
+      </p>
+      <table id="customers">
+        <tr>
+          <th id="a0">Sr.no.</th>
+          <th id="a1">Name and designation</th>
+          <th id="a2">E-mail</th>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Maria Anders</td>
+          <td>abc@gmail.com</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Christina Berglund</td>
+          <td>abc@gmail.com</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Francisco Chang</td>
+          <td>abc@gmail.com</td>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td>Roland Mendel</td>
+          <td>abc@gmail.com</td>
+        </tr>
+        <tr>
+          <td>5</td>
+          <td>Helen Bennett</td>
+          <td>abc@gmail.com</td>
+        </tr>
+        <tr>
+          <td>6</td>
+          <td>Philip Cramer</td>
+          <td>abc@gmail.com</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+  );
+}
