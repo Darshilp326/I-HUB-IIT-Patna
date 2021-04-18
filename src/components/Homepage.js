2f93a92d79781773f8443e6efa0eb445ece8c077
Sk044 <shubham_1901mm32@iitp.ac.in>
2021-04-19 01:38:16 +0530

    added calendar

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,11 +1,38 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import PicSlideShow from './PicSlideShow'
+import RevoCalendar from 'revo-calendar';
+import 'revo-calendar/dist/index.css';
+
 import Navbars from './Navbar/Navbar.js';
 import Footers from './Footer/Footer.js';
 
 import '../Styles/Homepage.css'
+
+
 export default function Homepage() {
+
+    var events = [
+        {
+          name: 'Buyout',
+          date: +new Date(),
+          allday: true
+        },
+        {
+          name: 'Reservation',
+          date: "April 22 2021",
+          extra: {
+            icon:
+              'M20.822 18.096c-3.439-.794-6.64-1.49-5.09-4.418 4.72-8.912 1.251-13.678-3.732-13.678-5.082 0-8.464 4.949-3.732 13.678 1.597 2.945-1.725 3.641-5.09           4.418-3.073.71-3.188 2.236-3.178 4.904l.004 1h23.99l.004-.969c.012-2.688-.092-4.222-3.176-4.935z',
+            text: '7 People coming for lecture' 
+          }
+        }
+      ]
+
+
+
+
+
     return (
         <div>
 
@@ -134,10 +161,49 @@ export default function Homepage() {
                 </Container>
 
             </section>
+
+            <br/><br/>
+            <br/><br/>
+            <br/><br/>
+            
            
             {/* Welcome page with text */ }
-            
+
+            <div style={{padding: "50px",}} >
+
+            <RevoCalendar
+
+                style={{
+                    borderRadius: "5px",
+                    boxShadow:"0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+                    }}
+                
+                events={events}
+
+                highlightToday={true}     
+                lang="en"
+                primaryColor="#216583"
+                secondaryColor="#D7E6EE"
+                todayColor="#3B3966"
+                textColor="#333333"
+                indicatorColor="orange"
+                animationSpeed={300}
+                sidebarWidth={180}
+                detailWidth={280}
+                showDetailToggler={true}
+                showSidebarToggler={true}
+                onePanelAtATime={false}
+                openDetailsOnDateSelection={true}
+                timeFormat24={true}
+                showAllDayLabel={false}
+                detailDateFormat="DD/MM/YYYY"
+                />
+                </div>
+
             {/* Calendar */ }
+            <br/><br/>
+            <br/><br/>
+            <br/><br/>
 
         </div>
     )
