0ba6973a9277794f4a8a3817660abdc83e76c06d
Darshilp326 <darshil_1901me18@iitp.ac.in>
2021-04-18 11:23:13 +0530

    Merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import { BrowserRouter, Route, Switch} from "react-router-dom";
 import Homepage from './components/Homepage'
-<<<<<<< HEAD
 import Startup from './components/Activity/Startup'
 import SkillDev from './components/Activity/SkillDev'
 import TechDev from './components/Activity/TechDev'
 import InternationalCollab from './components/Activity/InternationalCollab'
 import './App.css'
-=======
 import 'bootstrap/dist/css/bootstrap.min.css';
 
->>>>>>> 717e1fcf8590ab2f367e34579c8d4c4fb7bc2e0e
 function App() {
   return (
     <BrowserRouter>
